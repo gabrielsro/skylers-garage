a8f28d04b17cffb900fb121bb5a1666414b44196
Gabriel Rodriguez <gsrdz@tutanota.com>
2024-06-24 09:12:38 -0400

    Added Vercel's Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { inter } from "@/ui/fonts";
 import Navbar from "@/ui/home/navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
