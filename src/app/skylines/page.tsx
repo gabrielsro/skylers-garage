941a8bb074e03062b31cc2c4916d002da36eec32
Gabriel Rodriguez <gsrdz@tutanota.com>
2024-06-26 03:43:12 -0400

    Used route group for '/skylines/...'

--- src/app/skylines/page.tsx
+++ src/app/skylines/page.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <div className="grid place-content-center min-h-screen">
-      <p>Skylines</p>
+      <Link href="/skylines/all">
+        <div>
+          <p>All</p>
+        </div>
+      </Link>
+
+      <Link href="/skylines/r32">
+        <div>
+          <p>R32</p>
+        </div>
+      </Link>
+
+      <Link href="/skylines/r33">
+        <div>
+          <p>R33</p>
+        </div>
+      </Link>
+
+      <Link href="/skylines/r34">
+        <div>
+          <p>R34</p>
+        </div>
+      </Link>
     </div>
   );
 }
